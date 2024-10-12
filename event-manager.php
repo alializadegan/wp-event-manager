f55a7c818cafdd5b8951424a1eb4f42576f1fe66
Ali Alizadegan <alializadegan1999@gmail.com>
2024-10-12 12:06:03 +0330

    Update event-manager.php

--- event-manager.php
+++ event-manager.php
@@ -268,3 +268,9 @@ function filter_events_by_meta( $query ) {
     }
 }
 add_action( 'pre_get_posts', 'filter_events_by_meta' );
+
+function event_manager_load_textdomain() {
+    load_plugin_textdomain( 'event-manager', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+}
+add_action( 'plugins_loaded', 'event_manager_load_textdomain' );
+
